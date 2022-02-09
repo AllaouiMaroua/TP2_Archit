14e154b755979ae8187914bf956af03b8a3ec13d
AllaouiMaroua <allaoui.maroua10@gmail.com>
2022-02-09 16:09:47 +0100

    Question 12 13 14

--- src/EtudiantService.java
+++ src/EtudiantService.java
@@ -5,8 +5,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class EtudiantService implements IEtdSrv{
-	// Aprés avoir faire l'analyse ma solution proposée est de garder les responsabilités dans 
-         la méthode « inscription » car ils sont reliés entre eux temporellement.
+	/* Aprés avoir faire l'analyse ma solution proposée est de garder les responsabilités dans 
+         la méthode « inscription » car ils sont reliés entre eux temporellement */
 	public boolean inscription (int matricule, String nom, String prénom, String email,String pwd, int id_universite) throws SQLException	
 	{
 		EtudRepositoryInterf StudRep= new EtudiantRepository();
@@ -33,7 +33,21 @@ public class EtudiantService implements IEtdSrv{
 		 return true;
 	}
 	
-	
+	public void AddNbrBooksAllStudent (TypePackage pac) throws SQLException
+	{
+		 ArrayList<Etudiant> etudiants = GetEtudiatparLivreEmprunte();
+		 for (Etudiant E : etudiants) 
+		 {
+			 if (pac == TypePackage.Standard)
+		     {
+				 E.setNbLivreMensuel_Autorise(15);
+		     }
+		     else if (pac == TypePackage.Premium)
+		     {
+		    	 E.setNbLivreMensuel_Autorise(15*2);
+		     } 
+		}
+	}
 	
 	
 
